1ddf0a12d02409f7eff9d0c7183e15c89b75096c
Kelvinrodz13 <71534872+Kelvinrodz13@users.noreply.github.com>
2020-10-30 23:56:00 -0400

    Code Update
    Implementation of graphics in the gameView

---
+++ DrawPawns.py
@@ -0,0 +1,25 @@
+from graphics import *
+
+Blue_Pawns = [
+Circle(Point(0,0), 30).setFill("Blue"),
+Circle(Point(0,0), 30).setFill("Blue"),
+Circle(Point(0,0), 30).setFill("Blue"),
+Circle(Point(0,0), 30).setFill("Blue")]
+
+Yellow_Pawns = [
+Circle(Point(0,0), 30).setFill("Yellow"),
+Circle(Point(0,0), 30).setFill("Yellow"),
+Circle(Point(0,0), 30).setFill("Yellow"),
+Circle(Point(0,0), 30).setFill("Yellow")]
+
+Red_Pawns = [
+Circle(Point(0,0), 30).setFill("Red"),
+Circle(Point(0,0), 30).setFill("Red"),
+Circle(Point(0,0), 30).setFill("Red"),
+Circle(Point(0,0), 30).setFill("Red")]
+
+Green_Pawns = [
+Circle(Point(0,0), 30).setFill("Green"),
+Circle(Point(0,0), 30).setFill("Green"),
+Circle(Point(0,0), 30).setFill("Green"),
+Circle(Point(0,0), 30).setFill("Green")]
